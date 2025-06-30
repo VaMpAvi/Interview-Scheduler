9328007f49a5b04427f87bcf8963be6e09d4ceab
VampAvi <avinashbanyal31@gmail.com>
2025-06-30 16:06:27 +0530

    Email Service Implementation

--- src/main/java/com/example/interviewscheduler/InterviewSchedulerApplication.java
+++ src/main/java/com/example/interviewscheduler/InterviewSchedulerApplication.java
@@ -2,8 +2,10 @@ package com.example.interviewscheduler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class InterviewSchedulerApplication {
 
     public static void main(String[] args) {
